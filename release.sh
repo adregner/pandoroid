3ee4748bd2ffa644356dcf625210eb1482057566
Andrew Regner <andrew@aregner.com>
2011-03-29 14:56:37 -0400

    documentation and external publishing updates

--- release.sh
+++ release.sh
@@ -4,6 +4,6 @@
 PACKAGE_NAME=pandoroid #$(basename $(pwd))
 
 tar cvjf "../$PACKAGE_NAME.tar.bz2" --exclude-backups --exclude="consoleTesting" --exclude="bin" * .classpath .project .bzr*
-cp "bin/$PACKAGE_NAME.apk" ../
+cp bin/"$(basename $(pwd))".apk "../$PACKAGE_NAME.apk"
 
-scp "../$PACKAGE_NAME.apk" "../$PACKAGE_NAME.tar.bz2" doc/index.html aregner.com:public_html/android/"$PACKAGE_NAME"/
+scp "../$PACKAGE_NAME.apk" "../$PACKAGE_NAME.tar.bz2" doc/*.{html,png} aregner.com:public_html/android/"$PACKAGE_NAME"/
