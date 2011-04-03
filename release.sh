fdda97d58da6a64a3f3042d38eaa14dcf5c81da4
Andrew Regner <andrew@aregner.com>
2011-04-03 11:07:03 -0400

    push to github when we are ready to release

--- release.sh
+++ release.sh
@@ -3,7 +3,12 @@
 
 PACKAGE_NAME=pandoroid #$(basename $(pwd))
 
+# use this to fail if we try to publish without commited changes
+bzr dpush --strict --no-rebase || exit 1
+
+# bundle it all up
 tar cvjf "../$PACKAGE_NAME.tar.bz2" --exclude-backups --exclude="consoleTesting" --exclude="bin" * .classpath .project .bzr*
 cp bin/"$(basename $(pwd))".apk "../$PACKAGE_NAME.apk"
 
+# send it to the project's main site
 scp "../$PACKAGE_NAME.apk" "../$PACKAGE_NAME.tar.bz2" doc/*.{html,png} aregner.com:public_html/android/"$PACKAGE_NAME"/
