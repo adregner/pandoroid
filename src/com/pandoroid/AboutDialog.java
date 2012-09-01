dce420146c015234565879aca6e6e2ab63c594c4
Dylan Powers <dylan.kyle.powers@gmail.com>
2012-09-01 11:28:17 -0700

    About dialog update

--- src/com/pandoroid/AboutDialog.java
+++ src/com/pandoroid/AboutDialog.java
@@ -18,7 +18,12 @@
  */
 package com.pandoroid;
 
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.URLSpan;
 import android.widget.TextView;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.pandoroid.R;
@@ -28,7 +33,23 @@ public class AboutDialog extends SherlockActivity {
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
+		String version_name = "";
+		int build_num = 0;
+		try {
+			version_name = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+			build_num = getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
+		} catch (NameNotFoundException e) {}
 		
-		((TextView)findViewById(R.id.about_text)).setText(R.string.about);
+		String version_text = "Version " + version_name + " (" + build_num + ")";
+		SpannableString about_text = new SpannableString(getString(R.string.about) + 
+														 " (https://github.com/dylanPowers/pandoroid/wiki).");
+		int url_start = about_text.length() - 47;
+		int url_end = about_text.length() - 2;
+		about_text.setSpan(new URLSpan("https://github.com/dylanPowers/pandoroid/wiki"), 
+					       url_start, url_end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+		((TextView) findViewById(R.id.version_text)).setText(version_text);
+		TextView about_view = (TextView) findViewById(R.id.about_text);
+		about_view.setText(about_text);
+		about_view.setMovementMethod(LinkMovementMethod.getInstance());
 	}
 }
